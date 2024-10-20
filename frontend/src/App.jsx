148ecd96aa6ba50d02670fb65fcded8291e0f5f9
oto-webdev <r.otowebdev@gmail.com>
2024-10-20 15:37:06 +0400

    project completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,21 @@
-import React from 'react'
-import {Routes, Route} from 'react-router-dom'
-import CreateBook from './pages/CreateBook'
-import Home from './pages/Home'
-import DeleteBook from './pages/DeleteBook'
-import EditBook from './pages/EditBook'
-import ShowBook from './pages/ShowBook'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CreateBook from './pages/CreateBooks';
+import ShowBook from './pages/ShowBook';
+import EditBook from './pages/EditBook';
+import DeleteBook from './pages/DeleteBook';
 
 const App = () => {
   return (
-    <div className=''>
-      <Routes>
-        <Route path='/' element={<Home />} ></Route>
-        <Route path='/create' element={<CreateBook />} ></Route>
-        <Route path='/details/:id' element={<ShowBook />} ></Route>
-        <Route path='/edit:id' element={<EditBook />} ></Route>
-        <Route path='/delete/:id' element={<DeleteBook />} ></Route>
-      </Routes>
-    </div>
-  )
-}
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={<CreateBook />} />
+      <Route path='/books/details/:id' element={<ShowBook />} />
+      <Route path='/books/edit/:id' element={<EditBook />} />
+      <Route path='/books/delete/:id' element={<DeleteBook />} />
+    </Routes>
+  );
+};
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {BrowserRouter} from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { SnackbarProvider } from 'notistack';
 
-createRoot(document.getElementById('root')).render(
-  
-   <BrowserRouter>
-     <App />
-   </BrowserRouter>
-  
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <SnackbarProvider>
+      <App />
+    </SnackbarProvider>
+  </BrowserRouter>
+);
