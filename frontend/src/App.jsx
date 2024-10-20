c04766c9d4ab6ce76847a75bd02860c1ca1815a6
oto-webdev <r.otowebdev@gmail.com>
2024-10-20 15:07:03 +0400

    setup spa

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import CreateBook from './pages/CreateBook'
+import Home from './pages/Home'
+import DeleteBook from './pages/DeleteBook'
+import EditBook from './pages/EditBook'
+import ShowBook from './pages/ShowBook'
 
 const App = () => {
   return (
     <div className=''>
-      
+      <Routes>
+        <Route path='/' element={<Home />} ></Route>
+        <Route path='/create' element={<CreateBook />} ></Route>
+        <Route path='/details/:id' element={<ShowBook />} ></Route>
+        <Route path='/edit:id' element={<EditBook />} ></Route>
+        <Route path='/delete/:id' element={<DeleteBook />} ></Route>
+      </Routes>
     </div>
   )
 }
