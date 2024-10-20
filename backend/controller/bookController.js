96cc8089c8911780ec202c245486889746080ee2
oto-webdev <r.otowebdev@gmail.com>
2024-10-20 14:44:31 +0400

    updated book

--- backend/controller/bookController.js
+++ backend/controller/bookController.js
@@ -33,3 +33,36 @@ export const getBooks = async (req, res) => {
     }
 }
 
+export const getBook = async (req, res) => {
+    try{
+        const {id} = req.params;
+        const book = await Book.findById({_id: id})
+
+        return res.status(200).json(book)
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+}
+
+export const updateBook = async (req, res) => {
+    try{
+        if(
+            !req.body.title ||
+            !req.body.author ||
+            !req.body.publishYear
+        ){
+            return res.status(400).json({message: "All fields are required"})
+        }
+
+        const {id} = req.params;
+        const book = await Book.findByIdAndUpdate({_id: id}, req.body)
+
+        if(!book){
+            return res.status(404).json({message: "Invalid Id"})
+        }
+
+        return res.status(200).send({message: "Book updated successfully"})
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+}
--- backend/routes/bookRoute.js
+++ backend/routes/bookRoute.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { createBook, getBooks } from '../controller/bookController.js'
+import { createBook, getBook, getBooks, updateBook } from '../controller/bookController.js'
 
 const router = express.Router()
 
 router.post("/", createBook)
 router.get("/", getBooks)
+router.get("/:id", getBook)
+router.put("/:id", updateBook)
 
 export default router;
