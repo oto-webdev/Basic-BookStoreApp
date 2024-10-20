95276b0739a1a4763926815830b33cb5ede8abef
oto-webdev <r.otowebdev@gmail.com>
2024-10-20 14:57:41 +0400

    backend

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,11 @@ const app = express()
 const port = process.env.PORT || 4000;
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: 'localhost:4000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+}))
 app.use("/api/books", bookRouter)
 
 mongoose.connect(process.env.MONGO_URI).then(()=>{
